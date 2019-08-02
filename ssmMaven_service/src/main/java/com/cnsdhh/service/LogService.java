f2be5ecf40bff82368678c119479aef2ff0a32db
CNSDHH-nb <cnsdhh@live.com>
2019-08-02 11:38:33 +0800

    第二次大更新
    Signed-off-by: CNSDHH-nb <cnsdhh@live.com>

--- ssmMaven_service/src/main/java/com/cnsdhh/service/LogService.java
+++
@@ -1,9 +0,0 @@
-package com.cnsdhh.service;
-
-import com.cnsdhh.domain.Log;
-
-public interface LogService {
-
-    // 日志插入
-    public void insertLog(Log log);
-}
--- ssmMaven_service/src/main/java/com/cnsdhh/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cnsdhh.service;
-
-import com.cnsdhh.domain.User;
-
-public interface UserService {
-
-    // 查询登录用户
-    public User login(String username, String password);
-}
